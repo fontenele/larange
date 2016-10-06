d5a21eab18187a9c30e0d5caca80a37f10545c1b
Guilherme Fontenele <guilherme@fontenele.net>
2016-10-06 12:25:28 -0300

    Fixing composer

--- database/seeds/LarangeDatabaseSeeder.php
+++ database/seeds/LarangeDatabaseSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder {
+class LarangeDatabaseSeeder extends Seeder {
 
 	/**
 	 * Run the database seeds.
